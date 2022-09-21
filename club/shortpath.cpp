f2c197d63f3baefbfa5c4be33fe5ee48e77efee5
algasami <105358814+algasami@users.noreply.github.com>
2022-09-21 15:27:12 +0800

    ...

--- club/shortpath.cpp
+++ club/shortpath.cpp
@@ -1,8 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-unordered_map<char, set<pair<char,int>>> graph;
+typedef set<pair<char,int>> edge;
 
+unordered_map<char, edge> graph;
+unordered_map<char, pair<set<string>, int>> solutions;
+
+char start;
+
+void dfs(string currentPath, int weight, const char nextNode, const char destination)
+{
+  if(currentPath.find(nextNode) != string::npos)
+  {
+    return;
+  }
+  currentPath += nextNode; // append path
+  if(nextNode == destination)
+  {
+    // comparison between existing solutions and potential expansion
+    // Bron Kerbosch variant
+    if(solutions.find(destination) != solutions.end())
+    {
+      const pair<set<string>,int>& current = solutions[destination];
+      if(current.second > weight)
+      {
+        solutions[destination].first.clear();
+      } else if(current.second < weight)
+      {
+        return;
+      }
+    }
+    solutions[destination].first.insert(currentPath);
+    solutions[destination].second = weight;
+    return;
+  }
+  // Collect edges (next, weight)
+  for(auto& x:graph[nextNode])
+  {
+    dfs(currentPath, weight + x.second, x.first,destination);
+  }
+}
 
 int main(){
   ios_base::sync_with_stdio(0);
@@ -18,16 +55,24 @@ int main(){
     graph[a].insert(make_pair(b,weight));
     graph[b].insert(make_pair(a,weight));
   }
-
-  cout << "---Graph Debug---" << endl;
+  cin >> start;
   for(auto iter = graph.begin();iter!=graph.end();iter++)
   {
-    cout << iter->first << ": ";
-    for(auto& v:iter->second)
+    dfs("",0,start,iter->first);
+  }
+
+  for(auto iter = solutions.begin();iter!=solutions.end();iter++)
+  {
+    cout << start << "->" << iter->first << "'s shortest paths" << endl;
+    cout << "Smallest weight: " << iter->second.second << endl;
+    // cout << iter->first << "'s Solutions-------" << endl;
+    // cout << start << "至" << iter->first << "的最短解法---" << endl;
+    // cout << "最小權重: " << iter->second.second << endl;
+    for(auto n:iter->second.first)
     {
-      cout << v.first << "(" << v.second << ")";
+      cout << n << endl;
     }
-    cout << endl;
+    cout << "-----------" << endl;
   }
   return 0;
 }
